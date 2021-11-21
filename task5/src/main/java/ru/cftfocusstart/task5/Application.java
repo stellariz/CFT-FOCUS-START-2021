efc3a3dd336e0731c316aa5f62ff7cbe5626d2c4
Попов Руслан Дмитриевич <r.popov@g.nsu.ru>
2021-11-21 15:10:07 +0700

    Добавлено логирование исключений при инициализации данных с properties файла и проверка входных данных.

--- task5/src/main/java/ru/cftfocusstart/task5/Application.java
+++ task5/src/main/java/ru/cftfocusstart/task5/Application.java
@@ -1,9 +1,12 @@
 package ru.cftfocusstart.task5;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+@Slf4j
 public class Application {
     private static final int consumerCount;
     private static final int producerCount;
@@ -21,17 +24,40 @@ public class Application {
             producerTime = Integer.parseInt(properties.getProperty("PRODUCER_TIME"));
             storageSize = Integer.parseInt(properties.getProperty("STORAGE_SIZE"));
         } catch (IOException e) {
-            throw new IllegalArgumentException("Incorrect properties file");
+            throw new IllegalArgumentException("Incorrect properties file", e);
+        }
+    }
+
+    private static void validateInputData() {
+        if (consumerCount <= 0) {
+            throw new IllegalArgumentException("Consumer count can't be less than or equal to zero");
+        }
+        if (producerCount <= 0) {
+            throw new IllegalArgumentException("Producer count can't be less than or equal to zero");
+        }
+        if (consumerTime <= 0) {
+            throw new IllegalArgumentException("Consumer time can't be less than or equal to zero");
+        }
+        if (producerTime <= 0) {
+            throw new IllegalArgumentException("Producer time can't be less than or equal to zero");
+        }
+        if (storageSize <= 0) {
+            throw new IllegalArgumentException("Storage size can't be less than or equal to zero");
         }
     }
 
     public static void main(String[] args) {
-        Storage storage = new Storage(storageSize);
-        for (int i = 0; i < producerCount; ++i) {
-            new Thread(new Producer(i, storage, producerTime)).start();
-        }
-        for (int i = 0; i < consumerCount; ++i) {
-            new Thread(new Consumer(i, storage, consumerTime)).start();
+        try {
+            validateInputData();
+            Storage storage = new Storage(storageSize);
+            for (int i = 0; i < producerCount; ++i) {
+                new Thread(new Producer(i, storage, producerTime)).start();
+            }
+            for (int i = 0; i < consumerCount; ++i) {
+                new Thread(new Consumer(i, storage, consumerTime)).start();
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
     }
 }
